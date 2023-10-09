9eb8a2eb6797978ae07c94e8e9ca19150afde503
WilsonJorge <113472580+WilsonJorge@users.noreply.github.com>
2023-10-09 21:21:50 +0200

    refactor: creating user UserValidation

--- backend/middlewares/UserValidation.js
+++ backend/middlewares/UserValidation.js
@@ -2,10 +2,14 @@ const {body} =require("express-validator")
 
 const userCreateValidation = () =>{
     return[
-        body("name").isString().withMessage("O nome e obrigatorio.").isLength({min:3}).withMessage("O nome precisa ter no maximo 3 caracteres."),
-        body("email").isString().withMessage("O e-mail e Obrigatorio.").isEmail().withMessage("Insira o email Valido"),
-        body("password").isString().withMessage("A senha e obrigatoria").isLength({min:5}).withMessage("A senha precisa ter no maximo 5 caracteres."),
-        body("confirmpassword").isString().withMessage("A confirmacao de senha e obrigatoria.").custom((value,{req})=>{
+        body("name").isString().withMessage("O nome e obrigatorio.").isLength({min:3})
+        .withMessage("O nome precisa ter no maximo 3 caracteres."),
+        body("email").isString().withMessage("O e-mail e Obrigatorio.")
+        .isEmail().withMessage("Insira o email Valido"),
+        body("password").isString().withMessage("A senha e obrigatoria")
+        .isLength({min:5}).withMessage("A senha precisa ter no maximo 5 caracteres."),
+        body("confirmpassword").isString().withMessage("A confirmacao de senha e obrigatoria.").
+        custom((value,{req})=>{
             if(value != req.body.password){
                  throw new Error("As senhas nao sao iguais.");
             }
